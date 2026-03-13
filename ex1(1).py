3360517feebd12123f800d62521a5f477c96dae2
jasvanthreddya-29 <jasvanthreddy.a-29@scds.saiuniversity.edu.in>
2026-03-14 02:24:24 +0530

    Comment out circle area calculation code
    Commented out the existing code for calculating the area of a circle.

--- ex1(1).py
+++ ex1(1).py
@@ -7,19 +7,4 @@
 # else:
 #     print("invalid input:  Radius cannot be negative.")
 
-# import math
-# radius = float(input("Enter the radius of the circle"))
-# if radius >= 0:
-#     area = math.pi*(radius**2)
-#     print("the radius is non negative")
-#     print(f"the area is of the circle :{area}")
-# else:
-#     print("invalid input:radius cannot be negative")
-import math
-radius = float(input("Enter the radius of the circle:")) 
-if radius >= 0:
-    area = math.pi*(radius**2)
-    print("the radius is non negative")
-    print(f"enter the radius of the circle:{area}")
-else:
-    print("invalid input: radius cannot be negative") 
+
